c37b6faf256b46f4b6ccf8886f949900398b80d8
Vee0601 <gadisivhuhwavho@gmail.com>
2023-06-14 11:41:42 +0200

    second commit

--- greet.js
+++ greet.js
@@ -1,17 +1,4 @@
-describe('Test my greet function' , function(){
-    it("it should return 'Hello Vee' when called with 'Fabchi RJ'" , function(){
-        assert.equal("Hello, Vee", greet("Vee"), "this should not be true");
-    });
-
-
-
-    it("it should return 'Hello Vee' when called with 'Fabchi RJ'" , function(){
-        assert.equal("Hello, Vee", greet("Vee"), "this should not be true");
-    });
-
-
-
-    it("it should return 'Hello Vee' when called with 'Fabchi RJ'" , function(){
-        assert.equal("Hello, Vee", greet("Vee"), "this should not be true");
-    });
-});
+function greet(name){
+    return 'Hello, ' +name;
+    }
+    
---
+++ test/greet.tests.js
@@ -0,0 +1,17 @@
+describe('Test my greet function' , function(){
+    it("it should return 'Hello Vee' when called with 'Fabchi RJ'" , function(){
+        assert.equal("Hello, Vee", greet("Vee"), "this should not be true");
+    });
+
+
+
+    it("it should return 'Hello Vee' when called with 'Fabchi RJ'" , function(){
+        assert.equal("Hello, Vee", greet("Vee"), "this should not be true");
+    });
+
+
+
+    it("it should return 'Hello Vee' when called with 'Fabchi RJ'" , function(){
+        assert.equal("Hello, Vee", greet("Vee"), "this should not be true");
+    });
+});
