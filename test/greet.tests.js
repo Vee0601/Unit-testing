066d41600d2097cbbbbaed439651dbcd4882a3d1
Vee0601 <gadisivhuhwavho@gmail.com>
2023-06-14 11:48:00 +0200

    third commit

--- test/greet.tests.js
+++ test/greet.tests.js
@@ -1,17 +1,17 @@
 describe('Test my greet function' , function(){
-    it("it should return 'Hello Vee' when called with 'Fabchi RJ'" , function(){
+    it("it should return 'Hello Vee' when called with 'Gadisi'" , function(){
         assert.equal("Hello, Vee", greet("Vee"), "this should not be true");
     });
 
 
 
-    it("it should return 'Hello Vee' when called with 'Fabchi RJ'" , function(){
+    it("it should return 'Hello Vee' when called with 'Gadisi'" , function(){
         assert.equal("Hello, Vee", greet("Vee"), "this should not be true");
     });
 
 
 
-    it("it should return 'Hello Vee' when called with 'Fabchi RJ'" , function(){
+    it("it should return 'Hello Vee' when called with 'Gadisi'" , function(){
         assert.equal("Hello, Vee", greet("Vee"), "this should not be true");
     });
 });
